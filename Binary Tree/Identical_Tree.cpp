d37985cd9a20b49229fda5f0e0836a1e55c23717
Vishal yadav <153073153+iamvishalyadav2005@users.noreply.github.com>
2026-02-13 00:00:51 +0530

    Create Identical_Tree.cpp
    Implemented Identical Binary Tree

---
+++ Binary Tree/Identical_Tree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool isIdentical(TreeNode* p, TreeNode* q) {
+        // If both nodes are NULL
+        if (p == NULL && q == NULL)
+            return true;
+
+        // If one is NULL
+        if (p == NULL || q == NULL)
+            return false;
+
+        // Check current value and recursively check left and right
+        return (p->val == q->val) &&
+               isIdentical(p->left, q->left) &&
+               isIdentical(p->right, q->right);
+    }
+};
